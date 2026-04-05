8d775019a63c5ff752bc5882d3680fce0f26ed84
Madesh M <madeshdex10@gmail.com>
2026-04-05 18:25:04 +0530

    update the dashboard

--- src/routes/dashboard.ts
+++ src/routes/dashboard.ts
@@ -5,6 +5,38 @@ import { authMiddleware } from "../middleware/authMiddleware";
 
 const DashBoardRouter = express.Router();
 
-DashBoardRouter.get("/summary",  authMiddleware, AuthorizedRoles("viewer", "analyst", "admin"),getDashboardSummary);
+/**
+ * @swagger
+ * /dashboard/summary:
+ *   get:
+ *     summary: Get dashboard summary
+ *     tags: [Dashboard]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Dashboard summary fetched successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 totalUsers:
+ *                   type: number
+ *                 totalRecords:
+ *                   type: number
+ *                 totalRevenue:
+ *                   type: number
+ *       401:
+ *         description: Unauthorized
+ *       403:
+ *         description: Forbidden, insufficient role
+ */
+DashBoardRouter.get(
+  "/summary",
+  authMiddleware,
+  AuthorizedRoles("viewer", "analyst", "admin"),
+  getDashboardSummary
+);
 
 export default DashBoardRouter;
